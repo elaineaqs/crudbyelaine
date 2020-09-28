c9e78f8601e6b7e5be6cce6e14a7d7d50e889c46
Elaine Aquino <eaquino@pm.me>
2020-09-28 20:40:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# crudbyelaine
+ Uses PHP and Bulma
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+  <head>
+    <title>index</title>
+    <link rel="icon" type="image/x-icon" href="./assets/favicon.ico" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </head>
+
+  <body>
+  Grocery List
+
+Items
+
+Add
+
+Item    Edit    x
+Item    Edit    x
+Item    Edit    x
+
+Add
+  </body>
+</html>
